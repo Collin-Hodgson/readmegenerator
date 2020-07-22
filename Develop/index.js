8be534f4e2065b73e15a6032a4b91792b35de95d
Collin <collinhodgs@gmail.com>
2020-07-21 19:43:22 -0600

    Added input questions and inquirer/fs/util const

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,52 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    message: "Enter the title of your project.",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter a description for your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Enter instruction on how to install the project.",
+    name: "instruction",
+  },
+  {
+    type: "input",
+    message: "Enter usage informtion.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter all contributors to the project.",
+    name: "contributors",
+  },
+  {
+    type: "input",
+    message: "Enter testing instructions.",
+    name: "testing",
+  },
+  {
+    type: "input",
+    message: "What license would you like to use?",
+    name: "license",
+  },
+  {},
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
