4b221196e9f239e8ce39c152d29c5090dc083155
Collin <collinhodgs@gmail.com>
2020-07-25 09:57:43 -0600

    Formatting index.js like activity 40 in section 9 started adding readme format for function generatereadme

--- Develop/index.js
+++ Develop/index.js
@@ -2,70 +2,87 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
-// array of questions for user
-const questions = [
-  {
-    type: "input",
-    message: "Enter the title of your project.",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "Enter a description for your project.",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "Enter instruction on how to install the project.",
-    name: "instruction",
-  },
-  {
-    type: "input",
-    message: "Enter usage informtion.",
-    name: "usage",
-  },
-  {
-    type: "input",
-    message: "Enter all contributors to the project.",
-    name: "contributors",
-  },
-  {
-    type: "input",
-    message: "Enter testing instructions.",
-    name: "testing",
-  },
-  {
-    type: "input",
-    message: "What license would you like to use?",
-    name: "license",
-    choices: [
-      "GNU AGPLv3",
-      "GNU GPLv3",
-      "GNU LGPLv3",
-      "Mozilla",
-      "Apache",
-      "MIT",
-      "Boost",
-      "Unlicense",
-    ],
-  },
-  {
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "github",
-  },
-  {
-    type: "input",
-    message: "What is your email address?",
-    name: "email",
-  },
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// function to write README file
-function writeToFile(fileName, data) {}
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter the title of your project.",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Enter a description for your project.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter instruction on how to install the project.",
+      name: "instruction",
+    },
+    {
+      type: "input",
+      message: "Enter usage informtion.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Enter all contributors to the project.",
+      name: "contributors",
+    },
+    {
+      type: "input",
+      message: "Enter testing instructions.",
+      name: "testing",
+    },
+    {
+      type: "input",
+      message: "What license would you like to use?",
+      name: "license",
+      choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Mozilla",
+        "Apache",
+        "MIT",
+        "Boost",
+        "Unlicense",
+      ],
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+  ]);
+}
 
-// function to initialize program
-function init() {}
+function generateReadMe(answers) {
+  return `
+  # ${answers.title}
 
-// function call to initialize program
-init();
+  ![${answers.license}](https://img.shields.io/badge/License-${encodeURI(
+    answers.license
+  )}-green.svg)
+`;
+}
+
+promptUser()
+  .then(function (answers) {
+    const readme = generateReadMe(answers);
+
+    return writeFileAsync("ReadMe.md", readme);
+  })
+  .then(function () {
+    console.log("Successfully wrote to ReadMe.md");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
